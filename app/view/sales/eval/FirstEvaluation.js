a80db509972bc2c5b956fc9650f45683799d2fc3
Joe Nilson <joenilson@gmail.com>
2014-11-12 00:28:23 -0400

    Sales Eval Grid added tools and change columns to fit eval requeriments

---
+++ app/view/sales/eval/FirstEvaluation.js
@@ -0,0 +1,7 @@
+Ext.define('Uranium.view.sales.eval.FirstEvaluation', {
+    extend: 'Ext.panel.Panel',
+    initComponent: function(){
+        this.html = 'Hello';
+        this.callParent();
+    }
+});
---
+++ app/view/sales/eval/Survey.js
@@ -0,0 +1,7 @@
+Ext.define('Uranium.view.sales.eval.Survey', {
+    extend: 'Ext.panel.Panel',
+    initComponent: function(){
+        this.html = 'Bye';
+        this.callParent();
+    }
+});
